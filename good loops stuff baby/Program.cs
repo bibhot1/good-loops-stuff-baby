3fd57fdfb154adacdf0655741e54a00b2f4fd8a8
Bibhot1 <bndazbvu.me12@gmail.com>
2024-06-12 08:36:39 +1200

    Add project files.

---
+++ good loops stuff baby/Program.cs
@@ -0,0 +1,11 @@
+﻿// lets do for loop which counts up andprint hello world for 10 times
+for (int i = 0; i <=10 ; i++)
+{
+    Console.WriteLine("Hello I" + i);
+}
+
+// now lets do wnother for loop which counts down from 10 to 1 instead
+for (int i = 10; i >= 0; i--)
+{
+    Console.WriteLine("Hello I" + i);
+}
